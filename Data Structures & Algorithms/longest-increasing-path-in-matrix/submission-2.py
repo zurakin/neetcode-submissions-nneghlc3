61493a67a412d10fa62cac942090fb123c9b0d5e
ILYASS AZIRAR <52070629+zurakin@users.noreply.github.com>
2026-04-15 00:15:27 +0200

    Add: longest-increasing-path-in-matrix - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        if len(matrix) == 0:
+            return 0
+        mem = {}
+
+        def getNeighbors(y, x):
+            directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
+            neighbors = []
+            for dy, dx in directions:
+                if 0 <= y+dy < len(matrix) and 0 <= x+dx < len(matrix[0]) and matrix[y+dy][x+dx] > matrix[y][x]:
+                    neighbors.append((y+dy, x+dx))
+
+            # neighbors.sort(key= lambda pos: matrix[pos[0]][pos[1]])
+            return neighbors
+
+        def dfs(y, x):
+            if (y, x) in mem:
+                return mem[y, x]
+            res = 0
+            for neighbor in getNeighbors(y, x):
+                res = max(res, dfs(*neighbor))
+            res += 1
+            mem[(y, x)] = res
+            return res
+        res = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                res = max(res, dfs(i, j))
+        return res
+
+        
