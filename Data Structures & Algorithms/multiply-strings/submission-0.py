d7a4cf03900be7eab86212a2e0a2b76ab7eb2562
ILYASS AZIRAR <52070629+zurakin@users.noreply.github.com>
2026-04-18 22:03:27 +0200

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.py
@@ -0,0 +1,4 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # cheesy solution (will do the proper one if I feel like it)
+        return str(int(num1) * int(num2))
