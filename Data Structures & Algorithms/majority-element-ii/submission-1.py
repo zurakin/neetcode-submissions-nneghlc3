6e4cf026e6926c2d74105e79fe3aa9e6ff43cab1
ILYASS AZIRAR <52070629+zurakin@users.noreply.github.com>
2026-04-22 18:44:27 +0200

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        elements = {}
+        for n in nums:
+            if n in elements:
+                elements[n] += 1
+            elif len(elements) < 3:
+                elements[n] = 1
+            else:
+                keys = list(elements.keys())
+                for k in keys:
+                    elements[k] -= 1
+                    if elements[k] == 0:
+                        del elements[k]
+        survivors = set(elements.keys())
+        occurences = defaultdict(int)
+        
+        for e in nums:
+            if e in survivors:
+                occurences[e] += 1
+        print(occurences)
+
+        return [e for e in survivors if occurences[e] > len(nums)/3]
